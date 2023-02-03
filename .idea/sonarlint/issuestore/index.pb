8db0470d00023524b7584412683522c98a6e725b
eunchaeKang <eunchae.kang@kurlycorp.com>
2023-02-03 13:09:33 +0900

    test

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-?
-pages/index.tsx,b/3/b3357635c26d1b52053382ce7e8a8640d31fedc8
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
               mybeautybox
               가입이 완료되었습니다.
           </p>
-          <a href="https://www-qa3.stg.kurly.com/popup/partners/loreal/callback?statue=success">
+          <a href="https://www-qa3.stg.kurly.com/popup/partners/loreal/callback?status=success">
             확인(성공)
           </a>
           <a href="https://www-qa3.stg.kurly.com/popup/partners/loreal/callback">
