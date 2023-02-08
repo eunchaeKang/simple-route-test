0d36b3820cda801e6a6a33f194710b0d08f68a86
eunchaeKang <eunchae.kang@kurlycorp.com>
2023-02-08 15:24:23 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     const test = () => {
-        window.location.href="http://www-qa1.stg.kurly.com/popup/partners/loreal/callback?status=success"
+        window.location.href="http://www-qa4.stg.kurly.com/popup/partners/loreal/callback?status=success"
     }
 
     const test2 = () => {
-        window.open("http://www-qa4.st1.kurly.com/popup/partners/loreal/callback?status=success")
+        window.open("http://www-qa4.stg.kurly.com/popup/partners/loreal/callback?status=success")
     }
   return (
     <>
