fa57dcfcc3f5859fc1d9e23163b62799941164b8
eunchaeKang <eunchae.kang@kurlycorp.com>
2023-02-14 16:02:23 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     const test = () => {
-        window.location.href="http://localhost:3000/popup/partners/loreal/callback?status=success"
+        window.location.href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success"
     }
-
     const test2 = () => {
-        window.open("http://localhost:3000/popup/partners/loreal/callback?status=success")
+        window.location.href="https://local34.dev.kurly.com/popup/partners/loreal/callback?status=success"
     }
   return (
     <>
@@ -24,30 +23,11 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
           <p>
-              mybeautybox
-              가입이 완료되었습니다.
+              외부망 페이지 테스트
           </p>
 
-         <button type="button" onClick={test}>버튼 테스트1</button>
-          <button type="button" onClick={test2}>버튼 테스트2</button>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success">
-            확인(성공)
-          </a>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success" target="_self">
-              확인(성공, _self)
-          </a>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success" target="_blank" rel="noreferrer">
-              확인(성공, _black)
-          </a>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success" target="_parent">
-              확인(성공, _parent)
-          </a>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback?status=success" target="_top">
-              확인(성공, _top)
-          </a>
-          <a href="http://local34.dev.kurly.com/popup/partners/loreal/callback">
-            확인(실패)
-          </a>
+         <button type="button" onClick={test}>return url 테스트</button>
+          <button type="button" onClick={test2}>return url 테스트2</button>
       </main>
     </>
   )
